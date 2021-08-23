e665eb0592d1199a41422ea4c101351ac4aecdd1
eilla1 <git@ella.cx>
2021-08-23 11:04:09 -0700

    Add loops folder

---
+++ unit0/Loops/src/Main.java
@@ -0,0 +1,23 @@
+public class Main{
+  public static void main(String[] args) {
+    //We are going to practice some loop writing today using for loops, while loops, and print statements
+
+    //To print, use either System.out.print OR System.out.println
+
+    //From class - for loop syntax:
+    //for(initialization; conditional statement; increment/change)
+    
+    //Complete the following challenges using BOTH a for loop and a while loop
+
+    //1 - print all the integers 1 - 10, inclusive of both
+
+
+
+    //2 - print all the even integers between 1 and 1001
+
+
+
+    //3 - print all numbers divisible by 3, starting at 99, going from largest to smallest.
+
+  }
+}
