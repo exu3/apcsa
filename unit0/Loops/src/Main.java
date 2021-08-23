dbe4e9e8f37a6b6b437e4268dd98c9ce3951bbbe
eilla1 <git@ella.cx>
2021-08-23 11:38:41 -0700

    add solutions for For loops

--- unit0/Loops/src/Main.java
+++ unit0/Loops/src/Main.java
@@ -10,14 +10,17 @@ public class Main{
     //Complete the following challenges using BOTH a for loop and a while loop
 
     //1 - print all the integers 1 - 10, inclusive of both
-
-
+    for (int i = 1; i<=10; i++) {
+      System.out.println(i);
+    }
 
     //2 - print all the even integers between 1 and 1001
-
-
+    for (int x=2; x<=100; x+=2) {
+      System.out.println(x);
+    }
 
     //3 - print all numbers divisible by 3, starting at 99, going from largest to smallest.
-
+    for (int n = 99; n%3 == 0 && n>=3; n-- )
+      System.out.println(n);
   }
 }
