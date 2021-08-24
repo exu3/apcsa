66b264956885486809461e5d9d78380b768c7500
eilla1 <git@ella.cx>
2021-08-24 11:30:47 -0700

    :skull: make everything print on one line

--- unit0/Loops/src/Main.java
+++ unit0/Loops/src/Main.java
@@ -13,46 +13,58 @@ public class Main{
 
     // for loop
     for (int i = 1; i<=10; i++) {
-      System.out.println(i);
+      System.out.print(i);
     }
+    System.out.println();
+    System.out.println();
 
     // while loop
-
     int ii = 1;
     while (ii <=10) { 
-      System.out.println(ii);
+      System.out.print(ii);
       ii++;
     }
+    System.out.println();
+    System.out.println();
 
     //2 - print all the even integers between 1 and 1001
     // for loop
     for (int x=2; x<1001; x+=2) {
-      System.out.println(x);
+      System.out.print(x);
     }
+    System.out.println();
+    System.out.println();
 
     // while loop 
     int xx=2;
     while (xx<1001) {
-      System.out.println(xx);
+      System.out.print(xx);
       xx+=2;
     }
+    System.out.println();
+    System.out.println();
 
     //3 - print all numbers divisible by 3, starting at 99, going from largest to smallest.
     
     // for loop
       for (int n = 99; n>=3; n-- ) {
         if ( n%3 == 0 ) {
-          System.out.println(n);
+          System.out.print(n);
         }
     }
+    System.out.println();
+    System.out.println();
+
 
     // while loop
     int nn = 99;
     while (nn>=3) {
       if ( nn%3 == 0 ) {
-          System.out.println(nn);
+          System.out.print(nn);
         } 
       nn--;
     }
+    System.out.println();
+    System.out.println();
 }
 }
