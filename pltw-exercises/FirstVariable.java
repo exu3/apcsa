c9fa3350b3dd61d0dc1583f595af66880f3698b4
eilla1 <git@ella.cx>
2021-09-14 11:38:35 -0700

    add 1.1.2 activity

---
+++ pltw-exercises/FirstVariable.java
@@ -0,0 +1,20 @@
+/**
+ * Activity 1.1.2
+ */
+
+public class FirstVariable
+{
+ 	public static void main(String[] args)
+ 	{
+    int age = 12;
+    double allowance = 5.25;
+    boolean isOldEnough = true;
+ 
+    System.out.print("The value of age is ");
+    System.out.println(age);
+	System.out.print("The amount of allowance is $");
+	System.out.println(allowance);
+	System.out.print("Is the person old enough?");
+	System.out.println(isOldEnough);
+ 	}
+}
