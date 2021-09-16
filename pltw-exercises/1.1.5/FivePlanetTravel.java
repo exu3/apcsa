f8b94277addf863691fadec1c5a9a90399bef619
eilla1 <git@ella.cx>
2021-09-15 23:45:03 -0700

    WOrk on half of 1.1.5

---
+++ pltw-exercises/1.1.5/FivePlanetTravel.java
@@ -0,0 +1,35 @@
+/* 
+ * Activity 1.1.5
+ */
+public class FivePlanetTravel
+{
+  public static void main(String[] args)
+  {
+    // theplanets.org average distance from earth to the planets
+    int mercury = 56974146;
+    int venus = 25724767;
+    int mars = 48678219;
+    int jupiter = 390674710;
+    int saturn = 792248270;
+
+    // number of planets to visit
+    int numPlanets = 5;
+
+    // speed of light and our speed
+    int lightSpeed =  670616629;
+    lightSpeed /= 10;
+
+    // total travel time
+    double total = 0;
+
+    /* your code here */
+    System.out.println("distance to mercury " + (int)mercury);
+	System.out.println("distance to venus " + (int)venus);
+	System.out.println("distance to mars " + (int)mars);
+	System.out.println("distance to jupiter " + (int)jupiter);
+	System.out.println("distance to saturn " + (int)saturn);
+	// FINISH THIS PART THEN TURN IT IN IDK WHAT I"M DOING AAA"
+    
+    
+  }
+}
--- pltw-exercises/1.1.5/PlanetTravel2.java
+++ pltw-exercises/1.1.5/PlanetTravel2.java
@@ -1,16 +1,18 @@
-/**
+/*
  * Activity 1.1.5
- * have not starte dthi eyt
  */
-public class PlanetTravel2 {
-	public static void main(String[] args) {
-		// the planet's average distance from earth
-		int mars = 48678219;
-		// speed of light in mph
-		int lightSpeed = 670616629;
-		lightSpeed /=10;
+public class PlanetTravel2
+{
+  public static void main(String[] args)
+  {
+    // theplanets.org average distance from earth 
+    int mars = 48678219;
+     // speed of light in miles per hour
+    int lightSpeed =  670616629;
+    lightSpeed /= 10;
 
-		System.out.println("Travel time to Mars: " + mars/lightSpeed + " hours.");
-	}
-	
+	double calc =  mars;
+	calc /= lightSpeed;
+	System.out.println("Travel time to Mars: " + mars / (double)lightSpeed + " hours.");  }
 }
+
