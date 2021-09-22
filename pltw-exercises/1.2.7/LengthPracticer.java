560bb8be16f9b75344b0752522cccc1ce47d8fbe
eilla1 <git@ella.cx>
2021-09-21 22:49:18 -0700

    pain

---
+++ pltw-exercises/1.2.7/LengthPracticer.java
@@ -0,0 +1,12 @@
+/*
+ * Activity 1.2.7
+ */
+public class LengthPracticer
+{
+  public static void main(String[] args)
+  {
+   /* Your code here. */
+
+
+  }
+}
---
+++ pltw-exercises/1.2.7/LengthTester.java
@@ -0,0 +1,12 @@
+/*
+ * Activity 1.2.7
+ */
+public class LengthTester
+{
+  public static void main(String[] args)
+  {
+    String cat = "Figaro";
+    System.out.println(cat.length());
+  }
+}
+// example
