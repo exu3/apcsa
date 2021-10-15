d3626dd3811609afe1892bab87c709ccbabaa55a
eilla1 <git@ella.cx>
2021-10-14 22:00:39 -0700

    finish activity

--- pltw-exercises/3.6.1/Partners.java
+++ pltw-exercises/3.6.1/Partners.java
@@ -18,9 +18,12 @@ public class Partners
     // System.out.println(partners[0]);
     partners[1] = "Scrappy";
     partners[3] = "Wahoo Fish";
-    partners[5] = "Orpheus";
+    partners[5] = "Clippy";
 
-    System.out.println(partners[1]);
+    System.out.println(partners[1] +"\n" + partners[3] +"\n" + partners[5]+ "\n");
+
+    String[] friendPartners = partners;
+    System.out.println(friendPartners[1] + "\n" + friendPartners[3] + "\n" +friendPartners[5]);
   }
 }
 
