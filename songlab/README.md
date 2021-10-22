a6864c2c7a1672aebc1072a68482fd8603b5b68c
wahoo <hi@wahoo.fish>
2021-10-22 11:19:31 -0700

    wahoo

--- songlab/README.md
+++ songlab/README.md
@@ -1,4 +1,30 @@
 # Song Lab
 
-notes:
+## notes:
 - use scanner class to count the number of lines, divide by 4, use that number to set the size of the array
+
+## GC instructions: 
+
+We will be investigating some of the top songs from 1965 - 2015.
+
+
+Using the file attached, download and put it into your project (I will link VSCode and Replit projects tomorrow). You are to read in the data from the file INTO your project (using whatever method you like...yes we will talk about how this week).
+
+
+Format:
+song_name
+artist
+year
+number_of_words
+
+
+Once you have the data, you are to analyze and come up with the following:
+
+
+- Total number of songs
+- Average number of words per song 
+- Song with the most number of words
+- Song with the least number of words 
+- Artist with the longest name 
+- Artist that appears the most number of times (must be exact match) 
+- Does any song appear more than once? (must also be exact match)
--- songlab/SongLab.java
+++ songlab/SongLab.java
@@ -7,30 +7,52 @@ public class SongLab {
     {
       readData();
     }
+	// file has 20400 lines
+
+	/**
+	 * - Total number of songs
+		- Average number of words per song 
+		- Song with the most number of words
+		- Song with the least number of words 
+		- Artist with the longest name 
+		- Artist that appears the most number of times (must be exact match) 
+		- Does any song appear more than once? (must also be exact match)
+	 */
 
     public static void readData() {
-			String[] titleArray = new String[100];
-			String[] artistArray = new String[10];
-			int[] yearArray = new int[100];
-			int[] wcArray = new int[100];
+			String[] titleArray = new String[20400/4];
+			String[] artistArray = new String[20400/4];
+			int[] yearArray = new int[20400/4];
+			int[] wcArray = new int[20400/4];
+
+		    int numOfSongs = titleArray.length; // total number of songs
+			int avgWords = 0; // average number of words per song
+			String mostWords; // song with the most number of words
+			String leastWords; // song with the least number of words
+			String longestName; // artist with the longest name
+			String repeatedArtist; // artist that appears the most number of times
+			String repeatedSong; // song that appears more than once
 
         // try/catch block to catch file exception not found thing
         try {
-    // use scanner class to count the number of lines, divide by 4, use that number to set the size of the array
-					Scanner in = new Scanner(new File("./songs_new_line.txt"));           
+			Scanner in = new Scanner(new File("./songs_new_line.txt"));           
 
-					while (in.hasNextLine()) {
+			while (in.hasNextLine()) {
 
-						String title = in.nextLine();
-						String artist = in.nextLine();
-						String year = in.nextLine();
-						String numWords = in.nextLine();
-						System.out.println(title + "\n" + artist + "\n" + year + "\n" + numWords + "\n");
-											
-					}
+				String title = in.nextLine();
+
+				String artist = in.nextLine();
+
+				String year = in.nextLine();
+				String numWords = in.nextLine();
+				System.out.println(title + "\n" + artist + "\n" + year + "\n" + numWords + "\n");
+		
+			}
+
         } catch (Exception e) {
-          System.out.println("Check your file, mate.");
+            System.out.println("Check your file, mate.");
         }
+
 			}
 
 }
