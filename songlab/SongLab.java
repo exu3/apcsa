ef84dd3ff4b0481d174580b24f5db83fc4af95bf
eilla1 <git@ella.cx>
2021-10-26 11:28:36 -0700

    get most/leaset number of words for songs and artist

--- songlab/SongLab.java
+++ songlab/SongLab.java
@@ -28,13 +28,12 @@ public class SongLab {
 		    int numOfSongs = titleArray.length; // total number of songs
 			int avgWords = 0; // average number of words per song
 			int mostWords = 0;
-			int leastWords = 0;
+			int leastWords = 9999999;
 
-			String songMostWords; // song with the most number of words
-			String songLeastWords; // song with the least number of words
-			String longestName; // artist with the longest name
-			String repeatedArtist; // artist that appears the most number of times
-			String repeatedSong; // song that appears more than once
+			String songMostWords = ""; // song with the most number of words
+			String songLeastWords = ""; // song with the least number of words
+			String longestName = ""; // artist with the longest name
+			String repeatedSong = ""; // song that appears more than once
 
         // try/catch block to catch file exception not found thing
         try {
@@ -46,22 +45,49 @@ public class SongLab {
 				String artist = in.nextLine();
 				String year = in.nextLine();
 				String numWords = in.nextLine();
-			}
+
+
+				// song with the most # of words
+				if (Integer.parseInt(numWords) > mostWords) {
+					mostWords = Integer.parseInt(numWords);
+					songMostWords = title;
+				}
+
+				// song with the least # of words
+				if (Integer.parseInt(numWords) < leastWords) {
+					leastWords = Integer.parseInt(numWords);
+					songLeastWords = title;
+				}
+				
+				// artist with the longest name
+				if (artist.length() > longestName.length()) {
+					longestName = artist;
+				}
+				
 				// prints the contents of the file
 				// System.out.println(title + "\n" + artist + "\n" + year + "\n" + numWords + "\n"); 		
+			}
+
+			// most frequently appearing artist
+			String repeatedArtist = ""; // artist that appears the most number of times
+			int repeatedArtistCount = 0; // times this artist appears
+			
+			// for (int i = 0; i <= artistArray.length-1; i++) {
+			// 	int artistCounter = 0;
+			// 	for (int j = i+1; j <= artistArray.length-2; j++) {
+			// 		if ( artistArray[i].equals(artistArray[j]) ) {
+			// 			artistCounter++;
+			// 		}
+			// 		if ( artistCounter > repeatedArtistCount ) {
+			// 			repeatedArtistCount = artistCounter;
+			// 			repeatedArtist = artistArray[i];
+			// 		}
+			// 	}
+			// }
 
 				// artistArray[numOfSongs] = artist;
 				// titleArray[numOfSongs] = title;
 				// wcArray[numOfSongs] = title.length();
-				for (int i = 0; i < titleArray.length-1; i++) {
-					titleArray[i] = title;
-				}
-
-			
-			
-			for (int i = 0; i< titleArray.length; i++) {
-				System.out.println(titleArray[i]);
-			}
 
 				// artist with the longest name
 				// String artistLongestName;
@@ -72,15 +98,14 @@ public class SongLab {
 				// }
 				
 				// results
-				// System.out.println("Total number of songs: " + numOfSongs);
+				System.out.println("Total number of songs: " + numOfSongs);
 				// System.out.println("Avg # of words per song: " + numOfSongs);
-				// System.out.println("Song with the most words: " + numOfSongs);
-				// System.out.println("Song with the leat words:" + numOfSongs);
-				// System.out.println("Artist with the longest name: " + numOfSongs);
-				// System.out.println("Artist appearing the most # of times: " + numOfSongs);
+				System.out.println("Song with the most words: " + songMostWords + " (with " + mostWords + " words)");
+				System.out.println("Song with the least words:" + songLeastWords + " (with " + leastWords + " word)");
+				System.out.println("Artist with the longest name: " + longestName);
+				System.out.println("Artist appearing the most # of times: " + repeatedArtist + " (appearing " + repeatedArtistCount + " times)");
 				// System.out.println("Songs that appear more than once: " + numOfSongs);
-		
-			
+	
 
         } catch (Exception e) {
             System.out.println("Check your file, mate.");
