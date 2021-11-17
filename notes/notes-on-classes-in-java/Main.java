ba22922c1d47d895af3f1898e2cbdcc5f023afc7
eilla1 <git@ella.cx>
2021-11-17 11:26:55 -0800

    pltw thing

--- notes/notes-on-classes-in-java/Main.java
+++ notes/notes-on-classes-in-java/Main.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String [] args) {
-        Person one = new Person();
-        Person two = new Person();
-        Person three = new Person();
+        ArrayList<String> list = new ArrayList<String>();  
+
         
         Person four = new Person("Sandy", 50, true);
         // Person five = new Person("Joe", false); // cant do this because there's no age int
+        
+        list.add(new Person("Sandy", 50, true).toString());
+
+        System.out.println(list);
+   
     }
     
 }
