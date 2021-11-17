d8b9f61c75aa7b5d6e5f041aec4850808b5ff3b3
eilla1 <git@ella.cx>
2021-11-17 10:45:50 -0800

    Add notes yeah

---
+++ notes/notes-on-classes-in-java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String [] args) {
+        Person one = new Person();
+        Person two = new Person();
+        Person three = new Person();
+        
+        Person four = new Person("Sandy", 50, true);
+        // Person five = new Person("Joe", false);
+    }
+    
+}
---
+++ notes/notes-on-classes-in-java/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+
+	// Attributes of a person
+	// in APCSA, always make private instance variables
+	private int age;
+	private String name;
+	private boolean isAlive;
+
+	public Person() { // default constrcutor
+		name = "";
+		age = 0;
+		isAlive = false;
+	}
+
+	public Person(String name, int aage, boolean isAlive) {// overloaded methods / overloaded constructors
+		this.name = name; // use THIS yeah
+		this.age = age;
+		this.isAlive = isAlive;
+	}
+}
