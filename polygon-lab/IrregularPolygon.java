4d8f299cf11ed85e3deba6441688b00b3f46dbc5
eilla1 <git@ella.cx>
2021-12-08 11:21:06 -0800

    Half

--- polygon-lab/IrregularPolygon.java
+++ polygon-lab/IrregularPolygon.java
@@ -10,6 +10,7 @@ public class IrregularPolygon{
   // constructors
   public IrregularPolygon() { 
     myPencil = new DrawingTool(new SketchPad(500, 500));
+    myPolygon = new ArrayList <Point2D.Double> ();
   }
 
   // public methods
@@ -17,10 +18,57 @@ public class IrregularPolygon{
     myPolygon.add(aPoint);
   }
   public void draw() {
-    myPencil.draw();
+
+    myPencil.move(myPolygon.get(0).getX(), myPolygon.get(0).getY());
+    for (int i=0; i<myPolygon.size()-1; i++) {
+      Point2D.Double current = myPolygon.get(i);
+      Point2D.Double next = myPolygon.get(i+1);
+      System.out.println(current.getX());
+      System.out.println(current.getY());
+      double cx = current.getX();
+      double nx = next.getX();
+
+      double cy = current.getY();
+      double ny = next.getY();
+
+      
+
+      if (nx>cx) {
+        myPencil.forward(nx-cx);
+      } else if (nx<cx) {
+        myPencil.backward(cx-nx);
+      } else {
+        // nothing
+      }
+
+      if (ny>cy) {
+        myPencil.forward(ny-cy);
+      } else if (ny<cy) {
+        myPencil.backward(cy-ny);
+      } else {
+        // nothing
+      }
+    }
   }
+
   public double perimeter() { 
-    return 0;
+    double distance = 0;
+
+    for (int i=0; i<myPolygon.size()-1; i++) {
+      Point2D.Double current = myPolygon.get(i);
+      Point2D.Double next = myPolygon.get(i+1);
+      // System.out.println(current.getX());
+      // System.out.println(current.getY());
+      double cx = current.getX();
+      double nx = next.getX();
+
+      double cy = current.getY();
+      double ny = next.getY();
+
+      
+      distance = Math.abs(cx-nx) + Math.abs(cy-ny);
+    }
+    return distance;
   }
   public double area() { 
     return 0;
--- polygon-lab/Main.java
+++ polygon-lab/Main.java
@@ -8,7 +8,7 @@ public class Main{
 		testCase.add(new Point2D.Double(70, 20));
 		testCase.add(new Point2D.Double(50, 50));
 		testCase.add(new Point2D.Double(0, 40));
-		//uncomment when you are ready to test the perimeter area or draw
+		// uncomment when you are ready to test the perimeter area or draw
 		System.out.println("Perimeter: " + testCase.perimeter());
 		System.out.println("Area: " + testCase.area());
 		testCase.draw();
